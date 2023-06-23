c64b518c67d260dd78eb6b1d06f31b78bedac256
beark93 <beark93@gmail.com>
2023-06-23 19:46:27 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Header from './components/common/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <div className="content"></div>
+</template>
+
+<style scoped>
+.App .content {
+  margin: 20px auto;
+  width: 90%;
+  max-width: 1000px;
+  min-height: 70vh;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+.App .content .icon {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+  background-color: #ddd;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 80px;
+  margin: 10px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/common/Header.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<template>
+  <div className="header">
+    <h1>Kim BY 놀이터</h1>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background-color: #333;
+  margin: 0 auto;
+  width: 90%;
+  max-width: 1000px;
+  padding: 10px 20px;
+  color: #fff;
+  text-align: center;
+}
+
+.header h1 {
+  margin: 0;
+}
+</style>
