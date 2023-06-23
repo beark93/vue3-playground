e40f393e2402abdd6c603513694bd0ef2623ba5f
beark93 <beark93@gmail.com>
2023-06-23 20:17:03 +0900

    router 설치, Home 추가

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<template>
+  <div class="icon"></div>
+</template>
+
+<style scoped>
+.icon {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+  background-color: #ddd;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 80px;
+  margin: 10px;
+  cursor: pointer;
+}
+</style>
